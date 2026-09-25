9442916e216360c3e41f0335788a54be586761e4
philip-gibson <philip@ip-192-168-1-2.us-west-1.compute.internal>
2026-09-25 17:38:20 -0300

    fix(app): NavSideBar dark mode styling and component structure fix

---
+++ src/components/ButtonNavSideBarCollapse.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+  import { storeToRefs } from 'pinia'
+  import IconAngleLeft from '@primeicons/vue/angle-left'
+  import { useNavSideBarStore } from '@/stores/navSideBar'
+
+  const { isCollapsed } = storeToRefs(useNavSideBarStore())
+</script>
+
+<template>
+  <button
+    class="flex absolute right-[-0.75rem] top-[1rem] h-[1.5rem] w-[1.5rem] items-center justify-center
+      rounded-full border border-gray-200 bg-white shadow-sm hover:shadow-xl cursor-pointer"
+    type="button"
+    @click="isCollapsed = !isCollapsed">
+    <IconAngleLeft :class="{ 'rotate-180': isCollapsed }" :size="20" />
+  </button>
+</template>
--- src/components/NavSideBarEvents.vue
+++ src/components/NavSideBarEvents.vue
@@ -1,15 +1,14 @@
 <script lang="ts" setup>
+  import { storeToRefs } from 'pinia'
   import { computed } from 'vue'
   import { useRoute } from 'vue-router'
   import IconBook from '@primeicons/vue/book'
   import NavSideBarTab from '@/components/NavSideBarTab.vue'
   import NavSideBarText from '@/components/NavSideBarText.vue'
-  import type { NavSideBarState } from '@/components/NavSideBar.vue'
   import { RouteNames } from '@/router/types'
+  import { useNavSideBarStore } from '@/stores/navSideBar'
 
-  withDefaults(defineProps<NavSideBarState>(), {
-    collapsed: false,
-  })
+  const { isCollapsed } = storeToRefs(useNavSideBarStore())
 
   const route = useRoute()
 
@@ -21,7 +20,7 @@
     <RouterLink :to="{ name: RouteNames.events }" class="no-underline">
       <NavSideBarTab :selected="selected" class="justify-start">
         <IconBook :class="{ 'text-emerald-100': selected }" class="text-cyan-500 group-hover:text-emerald-100" size="24" />
-        <NavSideBarText v-if="!collapsed" :selected="selected">
+        <NavSideBarText v-if="!isCollapsed" :selected="selected">
           Events
         </NavSideBarText>
       </NavSideBarTab>
